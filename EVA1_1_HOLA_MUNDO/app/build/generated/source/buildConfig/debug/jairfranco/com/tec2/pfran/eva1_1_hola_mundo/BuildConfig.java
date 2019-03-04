840d44b3cc8561a2e379c5555c46205fd7623595
jairNX <37127039+jairNX@users.noreply.github.com>
2019-03-04 11:21:30 -0700

    Add files via upload

---
+++ EVA1_1_HOLA_MUNDO/app/build/generated/source/buildConfig/debug/jairfranco/com/tec2/pfran/eva1_1_hola_mundo/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package jairfranco.com.tec2.pfran.eva1_1_hola_mundo;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "jairfranco.com.tec2.pfran.eva1_1_hola_mundo";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
